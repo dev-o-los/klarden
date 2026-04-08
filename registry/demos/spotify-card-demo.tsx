01bfac101210a04a275f84bef75863b63ffa374b
devolos <utk27032005@gmail.com>
2026-04-08 15:38:36 +0530

    song changed

--- registry/demos/spotify-card-demo.tsx
+++ registry/demos/spotify-card-demo.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { SpotifyCard } from "@/registry/klarden-ui/spotify/spotify-card";
 
 export default function SpotifyCardDemo() {
   return (
     <div className="flex items-center justify-center p-4">
-      <SpotifyCard trackUrl="https://open.spotify.com/track/3sK8wGT43QFpWrvNQsrQya?si=17d74867a0a344e5" />
+      <SpotifyCard trackUrl="https://open.spotify.com/track/7EW7Yivb93qKAtp5qEm5of?si=b6de0dafaeaa4bfb" />
     </div>
   );
 }
